d4d44672bddeed6abda42e43603e332d8cc849f4
parsae80 <67233772+parsae80@users.noreply.github.com>
2022-05-12 13:10:41 +0430

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 import torch.functional as F
-import torch.nn.functional as NNF
+import torch.nn.functional as nnf
+from parsivar import Tokenizer, Normalizer
+import torchtext
 
-
-corpus = []
+corpus = open('shams_corpus.txt', encoding='utf8').read().split("\n")
 
 
 # incomplete splitting commas
 def tokenize_corpus(corpus):
-    tokens = [x.split() for x in corpus]
+    my_normalizer = Normalizer()
+    my_tokenizer = Tokenizer()
+    tokens = my_tokenizer.tokenize_sentences(my_normalizer.normalize(corpus))
     return tokens
 
 
 tokenized_corpus = tokenize_corpus(corpus)
-
-
 vocabulary = []
 for sentence in tokenized_corpus:
     for token in sentence:
@@ -39,14 +40,42 @@ for sentence in tokenized_corpus:
         # for each window position
         for w in range(-window_size, window_size + 1):
             context_word_pos = center_word_pos + w
-            # make sure not jump out sentence
+            # make sure not jump out of the sentence
             if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
                 continue
-            context_word_idx = indices[context_word_pos]
-            idx_pairs.append((indices[center_word_pos], context_word_idx))
+            idx_pairs.append((indices[center_word_pos], indices[context_word_pos]))
 
-words = torch.tensor([word2idx[w] for w in vocabulary], dtype=torch.long)
+words = torch.tensor([word2idx[w] for w in vocabulary], dtype=torch.long)  # putting words into a tensor
+one_hot_encoding = nnf.one_hot(words)
 
-one_hot_encoding = NNF.one_hot(words)
-print(vocabulary)
-print(one_hot_encoding)
+
+# input layer needed to be defined
+
+
+embedding_dims = 5
+W1 = Variable(torch.randn(embedding_dims, vocabulary_size).float(), requires_grad=True)
+W2 = Variable(torch.randn(vocabulary_size, embedding_dims).float(), requires_grad=True)
+num_epochs = 101
+learning_rate = 0.001
+
+for epo in range(num_epochs):
+    loss_val = 0
+    for data, target in idx_pairs:
+        x = Variable(one_hot_encoding).float()
+        y_true = Variable(torch.from_numpy(np.array([target])).long())
+
+        z1 = torch.matmul(W1, x)
+        z2 = torch.matmul(W2, z1)
+
+        log_softmax = nnf.log_softmax(z2, dim=0)
+
+        loss = nnf.nll_loss(log_softmax.view(1, -1), y_true)
+        loss_val += loss.data[0]
+        loss.backward()
+        W1.data -= learning_rate * W1.grad.data
+        W2.data -= learning_rate * W2.grad.data
+
+        W1.grad.data.zero_()
+        W2.grad.data.zero_()
+    if epo % 10 == 0:
+        print(f'Loss at epo {epo}: {loss_val/len(idx_pairs)}')
